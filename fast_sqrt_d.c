e18063dc371114928b62edd9cc6b41f602b5a158
gtheo <giorgostheodoraki@hotmail.com>
2024-06-19 09:11:16 +0300

    It works probably

--- fast_sqrt_d.c
+++ fast_sqrt_d.c
@@ -4,5 +4,19 @@
 double fast_sqrt_d(double x)
 {
 
+	double y;
+	long long i;
+
+	/* evil floating point bit level hacking */                      
+	i  = * ( long long * ) &x; 
 	
+	 /* what the fuck? */             
+	i  = 0x1FF7FFFDCFB74000LL + ( i >> 1 );
+
+	y  = * ( double * ) &i;
+	
+	y = y/2 + x/(2*y); /* 1st iteration */
+	y = y/2 + x/(2*y); /* 2nd iteration, this can be removed */
+
+	return y;
 }
